31cdb11384c98cb04173fe7c7f20ba193ed4ef24
Valentina Kozak <kozak_val@mail.ru>
2022-01-06 19:15:05 +0300

    make Classes task

--- main.js
+++ main.js
@@ -479,7 +479,84 @@ for (let i = 0; i < array3.length; i++) {
 
 
 
+// Работа с классами:
+const studentsData = [
+  {
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
+  },
+  {
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
+  },
+  {
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  }
+];
 
+class User {
+
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get fullname() {
+      return `${this.firstName} ${this.lastName}`
+  }
+}
+
+class Student extends User {
+  constructor(firstName, lastName, admissionYear, courseName) {
+      super(firstName, lastName)
+      this.admissionYear = admissionYear
+      this.courseName = courseName
+  }
+
+  get course() {
+    const currentYear = new Date().getFullYear()
+    return currentYear - this.admissionYear
+  }
+}
+
+class Students {
+  
+  constructor (students) {
+    this.students = students
+    this.data = []
+    this.students.forEach(student => {
+        const user = new Student(student.firstName, student.lastName, student.admissionYear, student.courseName)
+        this.data.push({
+            fullname: user.fullname,
+            courseName: user.courseName,
+            course: user.course
+        })
+    })
+  }
+
+  getInfo() {
+      return (this.data
+          .sort((a, b) => a.course - b.course)
+          .map(user => `${user.fullname} - ${user.courseName}, ${user.course} курс`))
+  }
+
+}
+
+const students = new Students(studentsData);
+console.log(students.getInfo());
 
 
 
