522725f3c80d788bf419c971dad021b62d74291e
Valentina Kozak <kozak_val@mail.ru>
2022-02-02 18:42:13 +0300

    make changes

--- TripHouse/main.js
+++ TripHouse/main.js
@@ -1,5 +1,4 @@
 // Top-section
-
 const filterPeople = document.querySelector('.children');
 const filterPeopleOptions = document.querySelector('.filter_wrapper')
 filterPeople.addEventListener('click', function() {
@@ -7,53 +6,119 @@ filterPeople.addEventListener('click', function() {
 });
 
 // Adults
+const filterAdultsMinus = document.querySelector('.filter_adults > .filter_plus-minus > .minus')
 const filterAdultsPlus = document.querySelector('.filter_adults > .filter_plus-minus > .plus')
 const filterAdultsNumber = document.querySelector('.filter_adults > .filter_plus-minus > .filter_number')
 let adultsNumber = 1;
 filterAdultsPlus.addEventListener('click', function() {
   adultsNumber++;
-  if (adultsNumber > 30) {
+  if (adultsNumber >= 30) {
     adultsNumber = 30
+    filterAdultsPlus.classList.add('plus_gray')
+  } else {
+    filterAdultsPlus.classList.remove('plus_gray')
+  }
+  if (adultsNumber > 1) {
+    filterAdultsMinus.classList.add('minus_blue')
   }
   filterAdultsNumber.innerHTML = adultsNumber;
 })
 
-const filterAdultsMinus = document.querySelector('.filter_adults > .filter_plus-minus > .minus')
 filterAdultsMinus.addEventListener('click', function() {
   adultsNumber--;
   if (adultsNumber < 1) {
     adultsNumber = 1
   }
+  if (adultsNumber > 1) {
+    filterAdultsMinus.classList.add('minus_blue')
+  } else {
+    filterAdultsMinus.classList.remove('minus_blue')
+  }
+  if (adultsNumber < 30) {
+    filterAdultsPlus.classList.remove('plus_gray')
+  }
   filterAdultsNumber.innerHTML = adultsNumber;
 })
 
 // Children
+const filterWrapper = document.querySelector('filter_wrapper')
+const filterChildrenMinus = document.querySelector('.filter_children > .filter_plus-minus > .minus')
 const filterChildrenPlus = document.querySelector('.filter_children > .filter_plus-minus > .plus')
 const filterChildrenNumber = document.querySelector('.filter_children > .filter_plus-minus > .filter_number')
 let childrenNumber = 0;
 filterChildrenPlus.addEventListener('click', function() {
   childrenNumber++;
-  console.log(childrenNumber)
-  if (childrenNumber > 30) {
-    childrenNumber = 30
+  if (childrenNumber >= 17) {
+    childrenNumber = 17
+    filterChildrenPlus.classList.add('plus_gray')
+  } else {
+    filterChildrenPlus.classList.remove('plus_gray')
+  }
+  if (childrenNumber >= 1) {
+    filterChildrenMinus.classList.add('minus_blue')
+    document.querySelector('.child_age_moreOne').style.display = 'block'
+    document.querySelector('.filter_wrapper').classList.add('filter_wrapper_select')
+  } 
+  if (childrenNumber >= 2) {
+    document.querySelector('.child_age').style.display = 'none'
+    document.querySelector('.age_select').classList.add('age_select_without-text')
+    document.querySelector('.filter_wrapper').classList.add('filter_wrapper_select_without-text')
   }
   filterChildrenNumber.innerHTML = childrenNumber;
 })
 
-const filterChildrenMinus = document.querySelector('.filter_children > .filter_plus-minus > .minus')
-const childAgeMoreOne = document.querySelector('.child_age_moreOne')
 filterChildrenMinus.addEventListener('click', function() {
   childrenNumber--;
-  if (childrenNumber < 0) {
+  if (childrenNumber <= 0) {
     childrenNumber = 0
-    filterChildrenMinus.classList.add('children_minus_moreOne')
-    childAgeMoreOne.style.display = 'none'
-  } else {
-    filterChildrenMinus.classList.remove('children_minus_moreOne')  
-    childAgeMoreOne.style.display = 'block'
+    filterChildrenMinus.classList.remove('minus_blue')
+    document.querySelector('.child_age_moreOne').style.display = 'none'
+    document.querySelector('.child_age').style.display = 'block'
+    document.querySelector('.filter_wrapper').classList.remove('filter_wrapper_select_without-text')
+    document.querySelector('.filter_wrapper').classList.remove('filter_wrapper_select')
+    document.querySelector('.age_select').classList.remove('age_select_without-text')
+  } 
+  if (childrenNumber < 17) {
+    filterChildrenPlus.classList.remove('plus_gray')
   }
   filterChildrenNumber.innerHTML = childrenNumber;
 })
+
+// Rooms
+const filterRoomsMinus = document.querySelector('.filter_rooms > .filter_plus-minus > .minus')
+const filterRoomsPlus = document.querySelector('.filter_rooms > .filter_plus-minus > .plus')
+const filterRoomsNumber = document.querySelector('.filter_rooms > .filter_plus-minus > .filter_number')
+let roomsNumber = 1;
+filterRoomsPlus.addEventListener('click', function() {
+  roomsNumber++;
+  if (roomsNumber >= 30) {
+    roomsNumber = 30
+    filterRoomsPlus.classList.add('plus_gray')
+  } else {
+    filterRoomsPlus.classList.remove('plus_gray')
+  }
+  if (roomsNumber > 1) {
+    filterRoomsMinus.classList.add('minus_blue')
+  }
+  filterRoomsNumber.innerHTML = roomsNumber;
+})
+
+filterRoomsMinus.addEventListener('click', function() {
+  roomsNumber--;
+  if (roomsNumber < 1) {
+    roomsNumber = 1
+  }
+  if (roomsNumber > 1) {
+    filterRoomsMinus.classList.add('minus_blue')
+  } else {
+    filterRoomsMinus.classList.remove('minus_blue')
+  }
+  if (roomsNumber < 30) {
+    filterRoomsPlus.classList.remove('plus_gray')
+  }
+  filterRoomsNumber.innerHTML = roomsNumber;
+})
+
 
 // Homes guests loves
 const data = [
