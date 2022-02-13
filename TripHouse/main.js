60f735533632ffa55f6416aeded7cfccb0d4bfa4
Valentina Kozak <kozak_val@mail.ru>
2022-02-13 17:23:10 +0300

    UPD after review

--- TripHouse/main.js
+++ TripHouse/main.js
@@ -223,60 +223,7 @@ filterRoomsMinus.addEventListener('click', function() {
   filterRoomsNumber.innerHTML = roomsNumber;
 })
 
-// Top-section_adaptive
-
-
 // Homes guests loves
-// const data = [
-//     {
-//       name: 'Hotel Leopold',
-//       city: 'Saint Petersburg',
-//       country: 'Russia',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-//     },
-//     {
-//       name: 'Apartment Sunshine',
-//       city: 'Santa  Cruz de Tenerife',
-//       country: 'Spain',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-//     },
-//     {
-//       name: 'Villa Kunerad',
-//       city: 'Vysokie Tatry',
-//       country: 'Slowakia',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-//     },
-//     {
-//       name: 'Hostel Friendship',
-//       city: 'Berlin',
-//       country: 'Germany',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-//     },
-//     {
-//       name: 'Radisson Blu Hotel',
-//       city: 'Kyiv',
-//       country: 'Ukraine',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-//     },
-//     {
-//       name: 'Paradise Hotel',
-//       city: 'Guadalupe',
-//       country: 'Mexico',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-//     },
-//     {
-//       name: 'Hotel Grindewald',
-//       city: 'Interlaken',
-//       country: 'Switzerland',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-//     },
-//     {
-//       name: 'The Andaman Resort',
-//       city: 'Port Dickson',
-//       country: 'Malaysia',
-//       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-//     },
-//   ];
 
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
@@ -321,14 +268,12 @@ function createCard(data) {
     </div>`
 };
 
-// data.forEach((element) => {
-//   swiper.appendSlide(createCard(element))
-// });
-
 fetch ('https://fe-student-api.herokuapp.com/api/hotels/popular')
   .then(response => response.json())
-  .then(data => {
-    data.forEach((element) => {
-      swiper.appendSlide(createCard(element))
-    });
-  })
+  .then(data => 
+    (function (){
+      data.forEach((element) => {
+        swiper.appendSlide(createCard(element))
+      })
+    }()));
+    
