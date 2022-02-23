9ee69246e65ec00ee7981cbfcb7f5547481e3716
Valentina Kozak <kozak_val@mail.ru>
2022-02-23 22:22:36 +0300

    add lesson 14, 15

--- Lesson 14, 15/main.js
+++ Lesson 14, 15/main.js
@@ -1,18 +1,25 @@
 // lesson-14
 const btn = document.getElementById('btn');
 const input = document.getElementById('input');
+const fData = new FormData;
 
-btn.addEventListener('click', () => {
+input.onchange = () => fData.append('file', input.files[0])
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
     fetch('https://fe-student-api.herokuapp.com/api/file', {
         method: 'POST',
         headers: {
             'Content-type': 'multipart/form-data'
         },
-        // body: new FormData(form)
+        body: fData
     }).then((response) => {
+        console.log(response.json())
         return response.json()
     }).then((data) => {
         console.log(data)
+    }).catch(err => {
+      console.log(err)
     })
 })
 
