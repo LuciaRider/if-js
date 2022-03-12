b04194e807c67673dd39b2c3235ea23bf99536cc
Valentina Kozak <kozak_val@mail.ru>
2022-03-12 12:29:13 +0300

    Reduce the number of requests to the server

--- TripHouse/main.js
+++ TripHouse/main.js
@@ -234,6 +234,20 @@ formButton.addEventListener('click', function() {
   .then(data => console.log(data))
 })
 
+// lesson-13
+function getRequest(url) {
+  const cachedValue = sessionStorage.getItem(url);
+  if (cachedValue) {
+    return Promise.resolve(JSON.parse(cachedValue))
+  } 
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      sessionStorage.setItem(url, JSON.stringify(data));
+      return data;
+    })
+}
+
 // Top-section_adaptive
 
 // Homes guests loves
